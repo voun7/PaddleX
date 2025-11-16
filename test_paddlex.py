92654290f7e69d78d53c14e42c8c3b265fe0e786
Victor N <nwaeze_victor@yahoo.com>
2025-11-15 22:40:13 -0500

    update

--- test_paddlex.py
+++ test_paddlex.py
@@ -1,34 +1,26 @@
 import os
-import subprocess
 from datetime import timedelta
-from pathlib import Path
 from time import perf_counter
 
+os.environ["PADDLE_PDX_MODEL_SOURCE"] = "BOS"
 os.environ["PADDLE_PDX_CACHE_HOME"] = r"C:\Users\Victor\Downloads\temp models"
 
-from test_paddleocr import PaddleOCR, TextDetection, TextRecognition
+from test_paddleocr import PaddleOCR
 
 start_time = perf_counter()
 
-ocr = PaddleOCR()
-text_det = TextDetection()
-text_rec = TextRecognition()
+img_files = r"C:\Users\Victor\OneDrive\Public\test images"
 
-img_files = Path(r"C:\Users\Victor\OneDrive\Public\test images")
-
-for img_file in img_files.iterdir():
-    print(img_file)
-    result = ocr.predict(str(img_file))
-    print(result)
-    result2 = text_det.predict(str(img_file))
+ocr_fn = PaddleOCR(
+    lang="it",
+    use_doc_orientation_classify=False, use_doc_unwarping=False, use_textline_orientation=False,
+)
+results = ocr_fn.predict_iter(img_files)
+for res in results:
+    print(res)
+    # res.save_to_img("output")
+    # res.save_to_json("output")
     print("-" * 200)
-    print(result2)
-    result3 = text_rec.predict(str(img_file))
-    print("-" * 200)
-    print(result3)
-    print("=" * 200)
-    break
-
-subprocess.run(["paddlex", "--install", "paddle2onnx"])
+    # break
 
 print(f"Duration: {timedelta(seconds=round(perf_counter() - start_time))}")
